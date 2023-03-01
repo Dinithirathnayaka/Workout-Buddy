58000998a65e66370ea22c30b2c54df40e0bd335
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2023-03-01 08:54:21 +0530

    Edit auth

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,28 +1,31 @@
 const express = require("express");
 const {
   createWorkout,
-  getWorkout,
   getWorkouts,
+  getWorkout,
   deleteWorkout,
   updateWorkout,
 } = require("../controllers/workoutController");
-const Workout = require("../models/WorkoutModel");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
 
-//GET all workouts
+// require auth for all workout routes
+router.use(requireAuth);
+
+// GET all workouts
 router.get("/", getWorkouts);
 
 //GET a single workout
 router.get("/:id", getWorkout);
 
-//POST a new workout
+// POST a new workout
 router.post("/", createWorkout);
 
-//DELETE a workout
+// DELETE a workout
 router.delete("/:id", deleteWorkout);
 
-//UPDATE a workout
+// UPDATE a workout
 router.patch("/:id", updateWorkout);
 
 module.exports = router;
